a21e5bd6106c92b6b902ef97a4992c5e09af5205
CHPTMT <tshedza.tshiololi34@gmail.com>
2023-09-11 15:02:52 -0700

    two

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,17 @@
-i
+#include <stdio.h>
+
+/**
+ * main - Print the alphabetic
+ * Return - Always (Success)
+ */
+
+int main(void)
+
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}	
